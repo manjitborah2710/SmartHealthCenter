9f1344cc2a70ae88132f3d6ce05fff598e76f03e
ManjiT <manjitborah2710@gmail.com>
2019-08-22 21:40:20 +0530

    added add_requisition_medicine

--- doctor/urls.py
+++ doctor/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('login',views.loginView,name='login-view'),
     path('home',views.homeView,name="doctor-home-view"),
     path('logout',views.log_out,name="logout-view"),
-    path('viewStaff',views.displayHealthCenterStaff,name="display-staff-view")
+    path('viewStaff',views.displayHealthCenterStaff,name="display-staff-view"),
+    path('viewRequisitionMedicine',views.displayRequisitionMedicine,name="display-requisitionmedicine-view"),
+    path('addRequisitionMedicine',views.addRequisitionMedicine,name="add-requisitionmedicine-view"),
+    path('insertrequisitionmedicine',views.insertIntoRequisitionMedicine,name="insert-into-requisitionmedicine-view")
 ]
