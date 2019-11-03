e13858262a7a5eb174b70b93c7ad391de2219efa
ManjiT <manjitborah2710.com>
2019-11-03 15:14:59 +0530

    removed cache

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/manjit/187A58267A57FF46/projects/final_year_atm/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
