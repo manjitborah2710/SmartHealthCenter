5df28f066b0817a4619ff50685ea9b8603b9b348
ManjiT <manjitborah2710.com>
2019-11-13 16:04:54 +0530

    changed database name

--- SmartHealthCenter/settings.py
+++ SmartHealthCenter/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'SmartHealthCenter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'final_manjit',
+        'NAME': 'finalproj_atm',
         'USER':'root',
         'PASSWORD':'password',
         'HOST':'localhost',
